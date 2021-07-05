60b66fc69e751f6cbfd2a2d21df86385ded9157a
arbias <arbiasgjoshi@gmail.com>
2021-07-05 17:14:13 +0200

    removed validation schema

--- src/components/organisms/contact-form/index.js
+++ src/components/organisms/contact-form/index.js
@@ -9,14 +9,10 @@ import { formWrapper, formRow, formBtn, formMask } from './contact-form.module.s
 const ContactForm = () => {
   const Intl = useIntl();
   const validationSchema = yup.object().shape({
-    username: yup
+    email: yup
       .string()
       .email('This field must be a valid email')
       .required('Email is a required field'),
-    password: yup
-      .string()
-      .min(6, 'Password must be at least 6 characters')
-      .required('Password is a required field'),
   });
 
   const contactUs = (val) => {
