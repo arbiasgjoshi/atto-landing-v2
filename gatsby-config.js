e172ad9c6da968cc685c01ced26711cae3ae7c3b
Ahmet Rama <ahmet@zetta.tech>
2021-08-30 15:08:51 +0200

    Testing csp directives

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,6 +121,7 @@ module.exports = {
           'img-src': "'self' data: www.google-analytics.com",
           'default-src': "'self' http: https:",
           'frame-src': "'self' https://vars.hotjar.com;",
+          'font-src': "'self' data:; https://hello.myfonts.net",
           'connect-src': "'self' https://analytics.google.com https://d3hb14vkzrxvla.cloudfront.net https://in.hotjar.com; https://stats.g.doubleclick.net;",
           // you can add your directives or override defaults
 
