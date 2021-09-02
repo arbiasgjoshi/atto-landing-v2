4d6dd42d2ed1eedd04cdfb048d64d288fa6ece77
arbias <arbiasgjoshi@gmail.com>
2021-09-02 14:28:10 +0200

    router redirects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Router } from '@reach/router';
+import { Router, Redirect } from '@reach/router';
 import HomePage from './home';
 import BlogTemplate from './blog-template';
 
@@ -8,6 +8,30 @@ const Index = () => (
   <Router basepath="/">
     <HomePage path="/" />
     <BlogTemplate path="/blog-template?slug=:slugId" />
+    <Redirect
+      from="https://attotime.com/download/android"
+      to="https://play.google.com/store/apps/details?id=tech.zetta.atto"
+    />
+    <Redirect
+      from="https://attotime.com/download/ios"
+      to="https://itunes.apple.com/us/app/atto-employee-time-location/id1132847984?ls=1&mt=8"
+    />
+    <Redirect
+      from="https://attotime.com/deep-links/open"
+      to="https://attotracking.page.link/open-app"
+    />
+    <Redirect
+      from="https://attotime.com/deep-links/timesheets"
+      to="https://attotracking.page.link/timesheets"
+    />
+    <Redirect
+      from="https://attotime.com/deep-links/invite"
+      to="https://attotracking.page.link/invite"
+    />
+    <Redirect
+      from="https://attotime.com/deep-links/settings"
+      to="https://attotracking.page.link/settings"
+    />
   </Router>
 );
 
