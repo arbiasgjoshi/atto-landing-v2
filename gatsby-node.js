9d9ed1abe6b38be110cc4bf7a3b2dc8baf9bf4d5
arbias <arbiasgjoshi@gmail.com>
2021-09-06 15:44:08 +0200

    fixed changes for blog styles

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,20 +2,8 @@ const path = require('path');
 
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions;
-  // console.log('Page -', page.path);
   if (page.path.match(/^\/app/)) {
-    // page.matchPath is a special key that's used for matching pages
-    // with corresponding routes only on the client.
     page.matchPath = '/*';
-    // Update the page.
     createPage(page);
   }
-
-  // if (page.path.match(/^\/blog/)) {
-  //   createPage({
-  //     path: '/blog',
-  //     matchPath: '/blog/*',
-  //     component: path.resolve(`./src/components/templates/articles/index.js`),
-  //   });
-  // }
 };
