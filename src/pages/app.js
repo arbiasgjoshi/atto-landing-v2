c2b70c0429ee99e2f1f5150fc5849a0ba80ca857
arbias <arbiasgjoshi@gmail.com>
2021-09-06 14:58:42 +0200

    removed the full url

--- src/pages/app.js
+++ src/pages/app.js
@@ -11,29 +11,17 @@ const App = () => (
     <Blog path="/blog" />
     <Expense path="/blog/:id" />
     <Redirect
-      from="https://attotime.com/download/android"
+      from="/download/android"
       to="https://play.google.com/store/apps/details?id=tech.zetta.atto"
     />
     <Redirect
-      from="https://attotime.com/download/ios"
+      from="/download/ios"
       to="https://itunes.apple.com/us/app/atto-employee-time-location/id1132847984?ls=1&mt=8"
     />
-    <Redirect
-      from="https://attotime.com/deep-links/open"
-      to="https://attotracking.page.link/open-app"
-    />
-    <Redirect
-      from="https://attotime.com/deep-links/timesheets"
-      to="https://attotracking.page.link/timesheets"
-    />
-    <Redirect
-      from="https://attotime.com/deep-links/invite"
-      to="https://attotracking.page.link/invite"
-    />
-    <Redirect
-      from="https://attotime.com/deep-links/settings"
-      to="https://attotracking.page.link/settings"
-    />
+    <Redirect from="/deep-links/open" to="https://attotracking.page.link/open-app" />
+    <Redirect from="/deep-links/timesheets" to="https://attotracking.page.link/timesheets" />
+    <Redirect from="/deep-links/invite" to="https://attotracking.page.link/invite" />
+    <Redirect from="/deep-links/settings" to="https://attotracking.page.link/settings" />
   </Router>
 );
 
