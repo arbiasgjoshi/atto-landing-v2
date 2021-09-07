5347338ea527945512329d21654974729c4da969
arbias <arbiasgjoshi@gmail.com>
2021-09-07 23:02:48 +0200

    commented redirects

--- src/pages/app.js
+++ src/pages/app.js
@@ -10,7 +10,7 @@ const App = () => (
     <HomePage path="/" />
     <Blog path="/blog" />
     <Expense path="/blog/:id" />
-    <Redirect
+    {/* <Redirect
       from="/download/android"
       to="https://play.google.com/store/apps/details?id=tech.zetta.atto"
     />
@@ -21,7 +21,7 @@ const App = () => (
     <Redirect from="/deep-links/open" to="https://attotracking.page.link/open-app" />
     <Redirect from="/deep-links/timesheets" to="https://attotracking.page.link/timesheets" />
     <Redirect from="/deep-links/invite" to="https://attotracking.page.link/invite" />
-    <Redirect from="/deep-links/settings" to="https://attotracking.page.link/settings" />
+    <Redirect from="/deep-links/settings" to="https://attotracking.page.link/settings" /> */}
   </Router>
 );
 
