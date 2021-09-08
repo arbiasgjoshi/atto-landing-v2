1be29b4bebb50fc1bde0ab76e72b774a925627ab
Ahmet Rama <ahmet@zetta.tech>
2021-09-08 15:32:54 +0200

    Testing gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,15 +131,11 @@ module.exports = {
         },
       },
     },
-    module.exports = {
-      plugins: [
-        {
-          resolve: `gatsby-plugin-styled-components`,
-          options: {
-            // Add any options here/
-          },
-        },
-      ],
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here/
+      },
     },
     {
       resolve: `gatsby-plugin-create-client-paths`,
