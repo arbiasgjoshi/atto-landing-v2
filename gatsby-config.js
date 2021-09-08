ef06662b08445ae3bec4c16b0225149f648d19d5
Ahmet Rama <ahmet@zetta.tech>
2021-09-08 15:10:52 +0200

    Testing csp directives

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,6 +133,16 @@ module.exports = {
         },
       },
     },
+    module.exports = {
+      plugins: [
+        {
+          resolve: `gatsby-plugin-styled-components`,
+          options: {
+            // Add any options here
+          },
+        },
+      ],
+    },
     {
       resolve: `gatsby-plugin-create-client-paths`,
       options: { prefixes: [`/app/*`] },
