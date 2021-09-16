198ff099da122989ee65935fe51b30132aa0199b
arbias <arbiasgjoshi@gmail.com>
2021-09-16 10:38:04 +0200

    added delayed to gatsby google tag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-tagmanager`,
+      resolve: `gatsby-plugin-google-tagmanager-delayed`,
       options: {
         id: 'GTM-NV2DTP3',
         includeInDevelopment: false,
